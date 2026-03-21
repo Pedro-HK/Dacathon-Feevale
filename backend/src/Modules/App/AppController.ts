44aa8418cb858f8c8f1640e5e3210d820e4b797e
PEDRO HENRIQUE KNORST <0415145@feevale.br>
2026-03-21 17:33:02 -0300

    remove unecessary things

--- backend/src/Modules/App/AppController.ts
+++ backend/src/Modules/App/AppController.ts
@@ -9,34 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Post('/create')
-  createItem(): string {
-    return this.appService.createItem();
-  }
-
-  @Post('/update')
-  updateItem(): string {
-    return this.appService.updateItem();
-  }
-
-  @Post('/delete')
-  deleteItem(): string {
-    return this.appService.deleteItem();
-  }
-
-  @Post('/read')
-  readItem(): string {
-    return this.appService.readItem();
-  }
-
-  @Post('/list')
-  listItems(): string {
-    return this.appService.listItems();
-  }
-
-  @Post('/search')
-  searchItems(): string {
-    return this.appService.searchItems();
-  }
 }
--- backend/src/Modules/discipline/discipline.service.ts
+++ backend/src/Modules/discipline/discipline.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Discipline } from './discipline.entity';
-import { DagNode, getAvailableDisciplines, getCriticalPath } from '../graph/dag';
 
 @Injectable()
 export class DisciplineService {
@@ -96,26 +95,5 @@ export class DisciplineService {
     });
     if (!discipline) throw new NotFoundException('Discipline not found');
     return discipline;
-  }
-
-  async getCourseGraph(course: 'cc' | 'si'): Promise<DagNode<string>[]> {
-    const disciplines = await this.disciplineRepository.find({
-      where: { course },
-      relations: ['prerequisites'],
-    });
-    return disciplines.map((discipline) => ({
-      id: discipline.id,
-      dependencies: discipline.prerequisites?.map((p) => p.id) ?? [],
-    }));
-  }
-
-  async getAvailable(course: 'cc' | 'si', completedIds: string[]): Promise<string[]> {
-    const graph = await this.getCourseGraph(course);
-    return getAvailableDisciplines(completedIds, graph);
-  }
-
-  async getCriticalPath(course: 'cc' | 'si'): Promise<string[]> {
-    const graph = await this.getCourseGraph(course);
-    return getCriticalPath(graph);
   }
 }
